dabd833f0d354cf46280bc3bb54f0502479539d4
Claus Due <claus@wildside.dk>
2012-12-30 23:25:33 +0100

    [TASK] Added ext_emconf.php

---
+++ ext_emconf.php
@@ -0,0 +1,50 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "tool".
+ *
+ * Auto generated 16-12-2012 02:31
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Tool: Services and Utilities for Extbase extensions',
+	'description' => 'A collection of Services (for injection) and Utilities (for static calling) for Extbase extensions.',
+	'category' => 'misc',
+	'author' => 'Claus Due',
+	'author_email' => 'claus@wildside.dk',
+	'author_company' => 'Wildside A/S',
+	'shy' => '',
+	'dependencies' => 'cms,extbase',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'beta',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 1,
+	'lockType' => '',
+	'version' => '2.0.0',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '4.5-0.0.0',
+			'cms' => '',
+			'extbase' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+			'fluidwidget' => '',
+		),
+	),
+	'suggests' => array(
+	),
+	'_md5_values_when_last_written' => '',
+);
+
+?>
