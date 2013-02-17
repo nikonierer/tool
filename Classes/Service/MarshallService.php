8d276453ea3b08a58c1e7833123c3dd299805af2
Claus Due <claus@wildside.dk>
2013-02-17 15:51:23 +0100

    [TASK] Improve PHP5.2 / TYPO3 4.5 support

--- Classes/Service/MarshallService.php
+++ Classes/Service/MarshallService.php
@@ -208,9 +208,17 @@ class Tx_Tool_Service_MarshallService implements t3lib_Singleton {
 		}
 		try {
 			$value = Tx_Extbase_Reflection_ObjectAccess::getProperty($instance, $propertyName, TRUE);
-		} catch (Exception $error) {
-			t3lib_div::sysLog('MarshallService encountered an error while attempting to retrieve the value of ' .
-				$className . '::$' . $propertyName, 'site - assuming safe deflation is possible', t3lib_div::SYSLOG_SEVERITY_NOTICE);
+		} catch (RuneimeException $error) {
+			try {
+				$getter = 'get' . ucfirst($propertyName);
+				if (FALSE === method_exists($instance, $getter)) {
+					return FALSE;
+				}
+			} catch (Exception $error) {
+				t3lib_div::sysLog('MarshallService encountered an error while attempting to retrieve the value of ' .
+					$className . '::$' . $propertyName . ' - assuming safe deflation is possible', 'site', t3lib_div::SYSLOG_SEVERITY_NOTICE);
+
+			}
 			return TRUE;
 		}
 		return (FALSE === $value instanceof Closure);
