c239c5e68cb80c9ed8da9aa1bc408fe607968302
Claus Due <claus@namelesscoder.net>
2013-12-04 19:36:47 +0100

    [TER] 1.1.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "tool".
  *
- * Auto generated 13-02-2013 23:25
+ * Auto generated 04-12-2013 19:36
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
-	'version' => '1.1.1',
+	'version' => '1.1.2',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '4.5.0-6.1.99',
@@ -43,7 +43,7 @@ $EM_CONF[$_EXTKEY] = array(
 	),
 	'suggests' => array(
 	),
-	'_md5_values_when_last_written' => 'a:16:{s:16:"ext_autoload.php";s:4:"cc5f";s:12:"ext_icon.gif";s:4:"68b4";s:9:"README.md";s:4:"bba4";s:33:"Classes/Resource/FileResource.php";s:4:"4519";s:46:"Classes/Resource/FileResourceObjectStorage.php";s:4:"cf9c";s:31:"Classes/Service/AuthService.php";s:4:"5392";s:32:"Classes/Service/CloneService.php";s:4:"a0c2";s:33:"Classes/Service/DomainService.php";s:4:"5638";s:31:"Classes/Service/FileService.php";s:4:"b2e7";s:31:"Classes/Service/JsonService.php";s:4:"2f8a";s:35:"Classes/Service/MarshallService.php";s:4:"3a3a";s:36:"Classes/Service/RecursionService.php";s:4:"5f6c";s:31:"Classes/Service/UserService.php";s:4:"66a2";s:32:"Classes/Utility/ArrayUtility.php";s:4:"25aa";s:31:"Classes/Utility/PathUtility.php";s:4:"92bf";s:34:"Classes/Utility/VersionUtility.php";s:4:"71ff";}',
+	'_md5_values_when_last_written' => 'a:18:{s:16:"ext_autoload.php";s:4:"e8df";s:12:"ext_icon.gif";s:4:"68b4";s:9:"README.md";s:4:"4f73";s:33:"Classes/Resource/FileResource.php";s:4:"4519";s:46:"Classes/Resource/FileResourceObjectStorage.php";s:4:"cf9c";s:31:"Classes/Service/AuthService.php";s:4:"d9b1";s:32:"Classes/Service/CloneService.php";s:4:"f809";s:33:"Classes/Service/DomainService.php";s:4:"5870";s:31:"Classes/Service/FileService.php";s:4:"4f16";s:31:"Classes/Service/JsonService.php";s:4:"0190";s:35:"Classes/Service/MarshallService.php";s:4:"b091";s:36:"Classes/Service/RecursionService.php";s:4:"5605";s:31:"Classes/Service/UserService.php";s:4:"66a2";s:32:"Classes/Utility/ArrayUtility.php";s:4:"3202";s:31:"Classes/Utility/PathUtility.php";s:4:"05cf";s:34:"Classes/Utility/VersionUtility.php";s:4:"b79b";s:33:"Documentation/ComplexityChart.png";s:4:"c574";s:30:"Documentation/PyramidChart.png";s:4:"1775";}',
 );
 
 ?>
